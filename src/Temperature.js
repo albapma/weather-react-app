4d7ca54bf102d3fb0b1d0b759cac5bd06f01f823
albapma <albaperezmejias@gmail.com>
2023-02-03 06:36:59 +0000

    updated temperature

--- src/Temperature.js
+++ src/Temperature.js
@@ -3,17 +3,19 @@ import Forecast from "./Forecast";
 
 export default function Temperature() {
   return (
-    <div class="d-flex weather-temperature">
-      <img
-        src="http://openweathermap.org/img/wn/10d@2x.png"
-        alt="description"
-        id="icon"
-      />
-      <div class="temperature-info">
-        <strong id="degrees">20 </strong>
-        <span class="units" id="celsius">
-          °C{" "}
-        </span>
+    <div>
+      <div class="d-flex weather-temperature">
+        <img
+          src="http://openweathermap.org/img/wn/10d@2x.png"
+          alt="description"
+          id="icon"
+        />
+        <div class="temperature-info">
+          <strong id="degrees">20 </strong>
+          <span class="units" id="celsius">
+            °C{" "}
+          </span>
+        </div>
       </div>
       <Forecast />
     </div>
